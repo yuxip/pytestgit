6266a7f41ac91eada3db547ba5f146fe9147afb3
Yuxi Pan <yuxipan@physics.ucla.edu>
2013-09-23 19:53:31 -0700

    initial project version

---
+++ script0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import sys
+print(sys.platform)
+print('hello world')
+print(2**100)
+x='Spmaa!'
+print(x*8)
+for x in 'Spam':
+	print(x)
+x='Spam!'
---
+++ script1.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+import script0
+print 'in script1.py'
+print(x)
